06c75db8ed20f2675a1e9e048878368196968e1a
Andreas Hontzia <honze-net@users.noreply.github.com>
2021-02-07 15:47:59 +0100

    Adding the example script.
    Working example script, that shows how to use the pattern.

---
+++ example.sh
@@ -0,0 +1,199 @@
+#!/usr/bin/env bash
+
+# Arch Linux Deployment
+# Example script for virtual environments
+# https://wiki.archlinux.org/index.php/installation_guide
+
+# Enable error handling.
+set -euxo pipefail
+
+# Enable logging.
+LOGFILE="install.log"
+exec &> >(tee -a "$LOGFILE")
+
+# Configuration options.
+# All variables should be exported/declared, so that they will be availabe in the arch-chroot.
+export KEYMAP="de-latin1"
+export LANGASDF="de_DE.UTF-8"
+export LOCALE="de_DE.UTF-8 UTF-8"
+export TIMEZONE="Europe/Berlin"
+export COUNTRY="Germany"
+export HOSTNAME="arc"
+export USERNAME="user"
+export PASSWORD=$USERNAME # It is not recommended to set production passwords here.
+export DISK="/dev/sda"
+
+# Find and set mirrors.
+# Note: This mirror list will be automatically copied into the installed system.
+pacman -Sy --needed --noconfirm reflector
+reflector --country $COUNTRY --age 12 --latest 10 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+
+# Use MBR, partition whole disk with one partition, bootable, no swap.
+parted -a optimal $DISK mklabel msdos mkpart primary 0% 100% set 1 boot on 
+
+# Get the "/dev/..." name of the first partition, format it and mount.
+export ROOTPARTITION=$(ls $DISK*1*) 
+mkfs.ext4 $ROOTPARTITION -L root
+mount $ROOTPARTITION /mnt/
+
+# Copy initial files and update fstab.
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Extend logging to persistant storage.
+cp "$LOGFILE" /mnt/root/
+exec &> >(tee -a "$LOGFILE" | tee -a "/mnt/root/$LOGFILE")
+
+# This function will be executed inside the arch-chroot.
+archroot() {
+  # Set and generate locales.
+  echo "LANG=$LANGASDF" >> /etc/locale.conf
+  echo "KEYMAP=$KEYMAP" >> /etc/vconsole.conf
+  sed -i "/$LOCALE/s/^#//" /etc/locale.gen # Uncomment line with sed
+  locale-gen
+
+  # Set time zone and clock.
+  ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+  hwclock --systohc
+
+  # Set hostname.
+  echo "$HOSTNAME" > /etc/hostname
+
+  # Create initramfs optional?
+  # mkinitcpio -P
+  
+  # Install boot loader.
+  pacman -S --needed --noconfirm grub
+  grub-install $DISK
+  grub-mkconfig -o /boot/grub/grub.cfg
+
+  # Install and enable network manager.
+  pacman -S --needed --noconfirm networkmanager
+  systemctl enable NetworkManager
+
+  # Install and configure sudo.
+  pacman -S --needed --noconfirm sudo
+  sed -i '/^# %wheel ALL=(ALL) NOPASSWD: ALL/s/^# //' /etc/sudoers # Uncomment line with sed
+
+  # Create a new user and add it to the wheel group.
+  useradd -m -G wheel $USERNAME
+  echo $USERNAME:$PASSWORD | chpasswd
+  passwd -e $USERNAME # Force user to change password at next login.
+  passwd -dl root # Delete root password and lock root account.
+
+  # Fix too low entropy, that can cause a slow system start.
+  pacman -S --needed --noconfirm haveged
+  systemctl enable haveged
+
+  # Install and configure the desktop environment.
+  pacman -S --needed --noconfirm gnome gnome-tweaks
+  systemctl enable gdm
+  sudo -u $USERNAME dbus-launch gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de')]"
+  sudo -u $USERNAME dbus-launch gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+  sudo -u $USERNAME curl -o /home/$USERNAME/pexels-photo-2387793.jpeg "https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg"
+  sudo -u $USERNAME dbus-launch gsettings set org.gnome.desktop.background picture-uri "/home/$USERNAME/pexels-photo-2387793.jpeg"
+
+  # Install git as prerequisite for the next steps.
+  pacman -S --needed --noconfirm git base-devel
+
+  # Install an AUR helper.
+  cd /tmp
+  sudo -u $USERNAME git clone https://github.com/actionless/pikaur
+  cd pikaur
+  sudo -u $USERNAME makepkg -fsri --noconfirm
+  cd .. && rm -R pikaur
+
+  # Install missing firmware. (Check if you need those. Probably for VirtualBox and VMware.)
+  cd /tmp
+  sudo -u $USERNAME git clone https://aur.archlinux.org/aic94xx-firmware.git
+  cd aic94xx-firmware
+  sudo -u $USERNAME makepkg -sri --noconfirm
+  cd .. && rm -R aic94xx-firmware
+
+  cd /tmp
+  sudo -u $USERNAME git clone https://aur.archlinux.org/wd719x-firmware.git
+  cd wd719x-firmware
+  sudo -u $USERNAME makepkg -sri --noconfirm
+  cd .. && rm -R wd719x-firmware
+
+  # Check if a hypervisor is used and install the corresponding guest software.
+  pacman -S --needed --noconfirm dmidecode 
+  if [[ $(dmidecode -s system-product-name) == *"VirtualBox"* ]]; then
+    pacman -S --needed --noconfirm linux-headers virtualbox-guest-utils
+    systemctl enable vboxservice
+  fi
+
+  if [[ $(dmidecode -s system-product-name) == *"VMware Virtual Platform"* ]]; then
+    pacman -S --needed --noconfirm linux-headers open-vm-tools xf86-video-vmware # Maybe gtkmm3, if needed.
+    systemctl enable vmtoolsd
+  fi
+
+  # Install some software. 
+  pacman -S --needed --noconfirm firefox firefox-i18n-de vim tmux man
+
+  # Install some software and append some options to the corresponding config file.
+  pacman -S --needed --noconfirm tilix
+  cat >> /home/$USERNAME/.bashrc << 'EOT' 
+if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+        source /etc/profile.d/vte.sh
+fi
+EOT
+
+  sudo -u $USERNAME pikaur -S --needed --noconfirm powerline powerline-fonts-git
+  cat >> /home/$USERNAME/.bashrc << 'EOT'
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. /usr/share/powerline/bindings/bash/powerline.sh
+EOT
+
+  # Append some options to config files.
+  cat >> /home/$USERNAME/.vimrc << 'EOT'
+let g:powerline_pycmd="py3"
+set rtp+=/usr/lib/python3.*/site-packages/powerline/bindings/vim
+set laststatus=2
+syntax enable
+EOT
+
+  cat >> /home/$USERNAME/.tmux.conf << 'EOT'
+set -g default-terminal "screen-256color"
+source /usr/lib/python3.*/site-packages/powerline/bindings/tmux/powerline.conf
+EOT
+
+  # Install and set an icon theme for Gnome.
+  sudo -u $USERNAME pikaur -S --needed --noconfirm paper-icon-theme-git
+  sudo -u $USERNAME dbus-launch gsettings set org.gnome.desktop.interface icon-theme 'Paper'
+
+  # This is an example of how to install packages by custom groups. Adapt as you like.
+  # The list below is an example for a pentest machine.
+  #declare -A PACKAGES
+  #PACKAGES[TOOLS]="dnsutils"
+  #PACKAGES[RECON]="nikto sslscan wireshark-qt exploitdb"
+  #PACKAGES[ENUM]="enum4linux sqlmap gobuster wfuzz wpscan"
+  #PACKAGES[SCANNING]="nmap masscan"
+  #PACKAGES[SPOOFING]="responder"
+  #PACKAGES[EXPLOITATION]="metasploit"
+
+  #for package in ${PACKAGES[@]}; do
+  #  sudo -u $USERNAME pikaur -S --needed --noconfirm $package \
+  #    || echo "$package" >> /home/$USERNAME/packages-with-errors.txt
+  #done
+  
+  # Reconfigure sudo, so that a password is need to elevate privileges.
+  sed -i '/^# %wheel ALL=(ALL) ALL/s/# //' /etc/sudoers # Uncomment line with sed
+  sed -i '/^%wheel ALL=(ALL) NOPASSWD: ALL/s/^/# /' /etc/sudoers # Comment line with sed
+  echo "Finished archroot." 
+}
+
+# Export the function so that it is visible by bash inside arch-chroot.
+export -f archroot
+arch-chroot /mnt /bin/bash -c "archroot" || echo "arch-chroot returned: $?"
+
+# Lazy unmount.
+umount -l /mnt
+
+cat << 'EOT'
+******************************************************
+* Finished. You can now reboot into your new system. *
+******************************************************
+EOT
